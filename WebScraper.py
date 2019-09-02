f98020d383c0e048d556c977790de78550523808
kgole <kartikgole77@gmail.com>
2019-09-01 19:49:00 -0500

    5. CSV Build

--- WebScraper.py
+++ WebScraper.py
@@ -14,11 +14,10 @@ for link in soup.find_all('a', attrs={'class': 'c-button'}):
     fullURL = "https://www.smartmobil.de"+str(eachLink)
     URLList.append(fullURL)
 
-
-
 del URLList[0:2]
 print(URLList)
-
+tuples = []
+tuple = {}
 for linkCrawl in URLList:
     source = requests.get(linkCrawl)
     plainText = source.text
@@ -27,32 +26,51 @@ for linkCrawl in URLList:
     UnlimitedSMS = 0
     #Loop for Smartphone titles
     for eachTitle in soup2.find_all('h1', {'class':'p-confi-cc-smartphone_selection-details-headline'}):
+
         device_name = eachTitle.text
-        print("Device name:- "+device_name)
+        #print("Device name:- "+device_name)
+        tuple["device_name"] = device_name
         #Loop for Memory
-        for eachTitle in soup2.find_all('span', {'class': 'p-confi-memory_picker-size'}):
-            device_storage = eachTitle.text.strip(' GB')
-            print("Storage:- "+device_storage)
+    for eachTitle in soup2.find_all('span', {'class': 'p-confi-memory_picker-size'}):
+        device_storage = eachTitle.text.strip(' GB')
+        #print("Storage:- "+device_storage)
+        tuple["device_storage"] = device_storage
         #Loop for Plan Name
-        for eachTitle in soup2.find_all('div', {'class': 'c-panel-headline', 'itemprop': 'name'}):
-            plan_name = eachTitle.text.strip('smartmobil.de')
-            print("Plan Name:- "+plan_name)
-            plan_data = plan_name.strip('LTE ')
-            print("Plan data:- "+plan_data.strip(' GB'))
+    for eachTitle in soup2.find_all('div', {'class': 'c-panel-headline', 'itemprop': 'name'}):
+        plan_name = eachTitle.text.strip('smartmobil.de')
+        #print("Plan Name:- "+plan_name)
+        tuple["plan_name"] = plan_name
+        plan_data = plan_name.strip('LTE ')
+        pdata_final = plan_data.strip(' GB')
+        tuple["plan_data"] = pdata_final
         #Loop for Monthly Price
-        for eachTitle in soup2.find_all('span', {'class': 'c-price-before_decimal'}):
-            totalPrice = eachTitle.text
-            Monthly_price = totalPrice+".99"
-            print(Monthly_price)
+    for eachTitle in soup2.find_all('span', {'class': 'c-price-before_decimal'}):
+        totalPrice = eachTitle.text
+        Monthly_price = totalPrice+".99"
+        #print(Monthly_price)
+        tuple["Monthly_price"] = Monthly_price
 
         #Loop for Unlimited Talk
-        for eachTitle in soup2.find_all('li', {'class': 'e-tarifbox-bulletpoints-phone'}):
-            if "300" in eachTitle.text:
-                Unlimitedtalk = False
-                UnlimitedSMS = False
-            else:
-                Unlimitedtalk = False
-                UnlimitedSMS = True
+    for eachTitle in soup2.find_all('li', {'class': 'e-tarifbox-bulletpoints-phone'}):
+        if "300" in eachTitle.text:
+            Unlimitedtalk = False
+            UnlimitedSMS = False
+            tuple["Unlimited_talk"] = Unlimitedtalk
+            tuple["Unlimited_SMS"] = UnlimitedSMS
+        else:
+            Unlimitedtalk = False
+            UnlimitedSMS = True
+            tuple["Unlimited_talk"] = Unlimitedtalk
+            tuple["Unlimited_SMS"] = UnlimitedSMS
 
-            print("Unlimited Talk?:- "+str(Unlimitedtalk))
-            print("Unlimited SMS?:- "+str(UnlimitedSMS))
+    tuples.append(tuple)
+    #print(tuple)
+
+
+print(tuples)
+filename = 'abc.csv'
+with open(filename, 'w') as f:
+    w = csv.DictWriter(f,['device_name','device_storage','plan_name','plan_data','Monthly_price','Unlimited_talk', 'Unlimited_SMS'])
+    w.writeheader()
+    for quote in tuples:
+        w.writerow(quote)
